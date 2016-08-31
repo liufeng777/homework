d1434e1a34eb6881b162b192bde6aed1f464fa10
liufeng7 <2334462970@qq.com>
2016-08-31 13:57:08 +0800

    0831trashCan

---
+++ trashCan/main.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Administrator on 2016/8/31 0031.
+ */
+$(function () {
+   $("#trash1").draggable({
+       scope:"group1",
+   });
+    $("#trashCan3").droppable({
+        scope:"group1",
+        drop:function () {
+            alert("成功");
+            $("div").remove("#trash1");
+        }
+    })
+    $("#trash2").draggable({
+        scope:"group2",
+    });
+    $("#trashCan1").droppable({
+        scope:"group2",
+        drop:function () {
+            alert("成功");
+            $("div").remove("#trash2");
+        }
+    })
+    $("#trash3").draggable({
+        scope:"group3",
+    });
+    $("#trashCan2").droppable({
+        scope:"group3",
+        drop:function () {
+            alert("成功");
+            $("div").remove("#trash3");
+        }
+    })
+});
