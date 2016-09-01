c72f9f2f23f4b4f46d25bd2d479cc1bc4b2bbb95
liufeng7 <2334462970@qq.com>
2016-09-01 08:09:20 +0800

    0831

--- 0831trashCan/main.js
+++ 0831trashCan/main.js
@@ -4,9 +4,11 @@
 $(function () {
    $("#trash1").draggable({
        scope:"group1",
+       revert:"invalid",
    });
     $("#trashCan3").droppable({
         scope:"group1",
+
         drop:function () {
             alert("成功");
             $("div").remove("#trash1");
@@ -15,9 +17,11 @@ $(function () {
 
     $("#trash2").draggable({
         scope:"group2",
+        revert:"invalid",
     });
     $("#trashCan1").droppable({
         scope:"group2",
+
         drop:function () {
             alert("成功");
             $("div").remove("#trash2");
@@ -26,6 +30,7 @@ $(function () {
     
     $("#trash3").draggable({
         scope:"group3",
+        revert:"invalid",
     });
     $("#trashCan2").droppable({
         scope:"group3",
