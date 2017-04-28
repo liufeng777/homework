cace973ae9c256bbeede767efd80b5154b902072
liufeng777 <liufeng_shanda@163.com>
2017-04-28 20:58:39 +0800

    add content to admin

--- node_Blog/routers/admin.js
+++ node_Blog/routers/admin.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
+
 const User = require('../models/User');
 const Category = require('../models/Category');
+const Content = require('../models/Content');
 
 //只有管理员才能进入后台管理页
 router.use(function(req, res, next) {
@@ -199,4 +201,139 @@ router.get('/category/delete', function(req, res) {
 	})
 })
 
+// 内容首页
+router.get('/content', function(req, res) {
+	let page = Number(req.query.page || 1);
+	const limitNum = 2;
+	let skipNum;
+	let totalPage;
+	Content.count().then(function(count) {
+		totalPage = Math.ceil(count / limitNum);
+		if(page > totalPage) {
+			page = totalPage;
+		}
+		if(page < 1) {
+			page = 1;
+		}
+		skipNum = (page - 1) * limitNum;
+		// populate('category')将content的category字段映射到Category模型上，获取对应的记录
+		Content.find().sort({ _id: -1 }).limit(limitNum).skip(skipNum).populate('category').then(function(contents) {
+			res.render('admin/content_index.html', {
+				userInfo: req.userInfo,
+				contents: contents,
+				page: page,
+				totalPage: totalPage,
+				part: 'content',
+			});
+		});
+	});	
+});
+// 添加内容
+router.get('/content/add', function(req, res) {
+	Category.find().then(function(categories) {
+		res.render('admin/content_add.html', {
+			userInfo: req.userInfo,
+			categories: categories,
+		});
+	});	
+});
+// 保存内容
+router.post('/content/add', function(req, res) {
+	if(req.body.category === '') {
+		res.render('admin/error.html', {
+			userInfo: req.userInfo,
+			errorInfo: '分类不能为空！',
+		});
+		return;
+	}
+	if(req.body.title === '') {
+		res.render('admin/error.html', {
+			userInfo: req.userInfo,
+			errorInfo: '标题不能为空！',
+		});
+		return;
+	}
+	new Content({
+		category: req.body.category,
+		title: req.body.title,
+		description: req.body.description,
+		content: req.body.content,
+	}).save(function() {
+		res.render('admin/success.html', {
+			userInfo: req.userInfo,
+			successInfo: '保存内容成功！',
+			url: '/admin/content',
+		})
+	})
+
+});
+// 编辑内容
+router.get('/content/edit', function(req, res) {
+	const id = req.query.id || '';
+	let categories = [];
+	Category.find().sort({ _id: 1}).then(function(rs) {
+		categories = rs;
+		return Content.findOne({
+			_id: id,
+		}).populate('category');
+	}).then(function(content) {
+		if(!content) {
+			res.render('admin/error.html', {
+				userInfo: userInfo,
+				errorInfo: '指定内容不存在',
+			});		
+		} else {
+			res.render('admin/content_edit.html', {
+				userInfo: req.userInfo,
+				content: content,
+				categories: categories,
+			});				
+		}
+	});	
+});
+// 保存编辑内容
+router.post('/content/edit', function(req, res) {
+	const id = req.query.id || '';
+	if(req.body.category === '') {
+		res.render('admin/error.html', {
+			userInfo: req.userInfo,
+			errorInfo: '分类不能为空！',
+		});
+		return;
+	}
+	if(req.body.title === '') {
+		res.render('admin/error.html', {
+			userInfo: req.userInfo,
+			errorInfo: '标题不能为空！',
+		});
+		return;
+	}
+	Content.update({
+		_id: id,
+	}, {
+		category: req.body.category,
+		title: req.body.title,
+		description: req.body.description,
+		content: req.body.content,
+	}).then(function() {
+		res.render('admin/success.html', {
+			userInfo: req.userInfo,
+			successInfo: '编辑内容成功！',
+			url: '/admin/content',
+		});
+	})
+});
+// 删除内容
+router.get('/content/delete', function(req, res) {
+	const id = req.query.id || '';
+	Content.remove({
+		_id: id,
+	}).then(function() {
+		res.render('admin/success.html', {
+			userInfo: req.userInfo,
+			successInfo: '删除内容成功！',
+			url: '/admin/content',
+		})
+	})
+});
 module.exports = router;
---
+++ node_Blog/schemas/contents.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+module.exports = new mongoose.Schema({
+	// 所属分类,分类的_id
+	category: {
+		// 类型
+		type: mongoose.Schema.Types.ObjectId,
+		// 引用 models中的Category
+		ref: 'Category',
+	},
+
+	// 标题
+	title: String,
+	// 简介
+	description: {
+		type: String,
+		default: '',
+	},
+	// 内容
+	content: {
+		type: String,
+		default: '',
+	},
+});
