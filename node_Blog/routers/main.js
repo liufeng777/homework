37b7e7ab21c5cd7fe154cc9fc0d7ad7d0c9115a4
liufeng777 <liufeng_shanda@163.com>
2017-05-05 20:08:22 +0800

    detail of the content

--- node_Blog/routers/main.js
+++ node_Blog/routers/main.js
@@ -3,25 +3,29 @@ const router = express.Router();
 const Category = require('../models/Category');
 const Content = require('../models/Content');
 
+// 处理通用数据
+const data = {};
+router.use(function(req, res, next) {
+	data.userInfo = req.userInfo;
+	data.categories = [];
+	Category.find().then(function(categories) {
+		data.categories = categories;
+		next();
+	});
+})
 // res.render中的路径是相对views而言的
-router.get('/', function (req, res, next) {
-	const data = {
-		userInfo: req.userInfo,
-		categories: [],
-		category: req.query.category || '',
-		count: 0,
-		page: Number(req.query.page || 1),
-		limitNum: 2,
-		totalPage: 0,
-	};
+router.get('/', function (req, res, next) {	
+	data.category = req.query.category || '';
+	data.count = 0;
+	data.page = Number(req.query.page || 1);
+	data.limitNum = 2;
+	data.totalPage = 0;
+
 	const where = {};
 	if(data.category) {
 		where.category = data.category;
 	}
-	Category.find().then(function(categories) {
-		data.categories = categories;
-		return Content.where(where).count();
-	}).then(function(count){
+	Content.where(where).count().then(function(count){
 		data.count = count;
 		data.totalPage = Math.ceil(data.count / data.limitNum);
 		if(data.page > data.totalPage) {
@@ -37,5 +41,18 @@ router.get('/', function (req, res, next) {
 		res.render('main/index.html', data);
 	});		
 });
+
+router.get('/detail', function(req, res, next) {
+	const contentId = req.query.contentId;
+	Content.findOne({
+		_id: contentId,
+	}).populate('author').then(function(content) {
+		data.content = content;
+		// 阅读量加1
+		content.views ++;
+		content.save();
+		res.render('main/detail.html', data);
+	})
+})
 
 module.exports = router;
